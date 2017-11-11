f1646fcf73ebde05df3f52f52a54ae593afab0ff
Grady L Shelton <gradyish@gmail.com>
2017-11-10 19:33:31 -0800

    add react-redux to node modules

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
+
+const Valet_Buddy = () => (
+  <App />
+);
 
 AppRegistry.registerComponent('Valet_Buddy', () => App);
